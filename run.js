965db73a79edf6b645e1ccccc9e696c53d95cf7b
Nikolay Kostyurin <jilizart@gmail.com>
2015-09-21 02:37:15 +0200

    es6 convert

--- run.js
+++ run.js
@@ -1,21 +1,16 @@
-'use strict';
+import Ghost from './lib/Ghost';
 
-var Ghost = require('./lib/Ghost');
-
-var g = new Ghost();
+const ghost = new Ghost();
 
 // block for 50ms on all sockets - if you intend to perform any timed actions more frequently you should change this
-// that said it's likely we'll loop more often than this due to there being data waiting on one of the sockets but there aren't any guarantees
-
-var id = setInterval(function () {
-	if (g.update()) {
-		clearInterval(id);
+// that said it's likely we'll loop more often than this
+// due to there being data waiting on one of the sockets but there aren't any guarantees
+const tick = () => {
+	if (ghost.update()) {
+		clearTimeout(id);
+	} else {
+		tick();
 	}
-}, 50);
+};
 
-/**
-var BNet = require('./lib/BNet');
-var bn = new BNet('server.eurobattle.net', 'eurobattle', 6112);
-
-bn.run();
-*/
+const id = setTimeout(tick, 50);
