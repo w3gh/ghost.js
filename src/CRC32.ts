46b6395ce46faf3eced7388610448f3299b2c738
Nikolay Kostyurin <jilizart@gmail.com>
2019-07-29 00:26:46 +0200

    feat: implemented crc32

--- src/CRC32.ts
+++ src/CRC32.ts
@@ -1,58 +1,7 @@
+import crc32 from 'crc/lib/es6/crc32';
 
 export class CRC32 {
-    ulTable = new Uint32Array(256);
-
-    CRC32_POLYNOMIAL = 0x04c11db7;
-
-    constructor() {
-        this.init();
-    }
-
-    init() {
-        let ulTable = this.ulTable;
-
-        for (let iCodes = 0; iCodes <= 0xFF; ++iCodes) {
-            ulTable[iCodes] = this.reflect(iCodes, 8) << 24;
-
-            for (let iPos = 0; iPos < 8; iPos++)
-                ulTable[iCodes] = ( ulTable[iCodes] << 1 ) ^ ( ulTable[iCodes] & (1 << 31) ? this.CRC32_POLYNOMIAL : 0 );
-
-            ulTable[iCodes] = this.reflect(ulTable[iCodes], 32);
-        }
-    }
-
-    reflect(ulReflect: number, cChar: number) {
-        let ulValue = 0;
-
-        for (let iPos = 1; iPos < ( cChar + 1 ); ++iPos) {
-            if (ulReflect & 1)
-                ulValue |= 1 << ( cChar - iPos );
-
-            ulReflect >>= 1;
-        }
-
-        return ulValue;
-    }
-
-    fullCRC(sData: Buffer, ulLength: number) {
-        let ulCRC = 0xFFFFFFFF;
-
-        ulCRC = this.partialCRC(ulCRC, sData, ulLength);
-
-        const uints = new Uint32Array([ulCRC ^ 0xFFFFFFFF]);
-
-        return uints[0]
-    }
-
-    partialCRC(ulInCRC: number, sData: Uint8Array, ulLength: number) {
-        let ulTable = this.ulTable;
-        let initCRC = ulInCRC;
-
-        for (let index = 0; index < ulLength; index++) {
-            const byte = sData[index];
-            initCRC = ulTable[(initCRC ^ byte) & 0xff] ^ initCRC >>> 8;
-        }
-
-        return initCRC
+    fullCRC(sData: Buffer) {
+        return crc32(sData)
     }
 }
--- src/tests/crc32/crc32-js.ts
+++ src/tests/crc32/crc32-js.ts
@@ -1,21 +1,19 @@
 import {CRC32} from "../../CRC32";
+
 import fs = require('fs');
-import {ByteUInt32, ByteExtractString, ByteDecodeToString, ByteToArrayBuffer} from "../../Bytes";
+import {ByteUInt32, ByteDecodeToString} from "../../Bytes";
 
 function main() {
     const argv = process.argv;
 
     if (argv.length > 2) {
         const m_CRC = new CRC32();
-        m_CRC.init();
 
         if (fs.existsSync(argv[2])) {
             const m_MapData = fs.readFileSync(argv[2]);
-            const crc = m_CRC.fullCRC(
-                m_MapData,
-                m_MapData.length
-            );
+            const crc = m_CRC.fullCRC(m_MapData);
             const MapInfo = ByteUInt32(crc);
+
             console.log(crc);
             console.log(ByteDecodeToString(MapInfo));
             process.exit(0)
